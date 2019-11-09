ca9cfc9b9ba77bb967ee60a93dde2b6f16d051ba
风很大 <1832355298@qq.com>
2019-11-09 08:38:26 +0800

    14585

--- docs/.vuepress/components/css/webActive/Active2.vue
+++ docs/.vuepress/components/css/webActive/Active2.vue
@@ -1,7 +1,6 @@
 <template>
     <div class="active">
-        <div class="all">
-            <img src="https://s2.ax1x.com/2019/11/08/MZVdXt.jpg" alt="MZVdXt.jpg" border="0" />        </div>
+            <img src="https://s2.ax1x.com/2019/11/08/MZVdXt.jpg" alt="MZVdXt.jpg"  />
     </div>
 </template>
 
@@ -12,9 +11,6 @@
 </script>
 
 <style scoped>
-.all{ width: 740px; margin: 0 auto;}
-.more{overflow: hidden;width: 200px;margin: 0 auto;text-align: center;cursor: pointer;}
-.more a{text-decoration: none; display: block;background: #c07fe7;color: #fff;font-size: 15px;margin: 55px auto 80px;padding: 10% 0;-webkit-transition: all 2s ease;transition: all 2s ease;}
-.more a:hover{background-color: black; text-decoration: none !important;}
+.active img{ margin: 0 auto; display: block;}
 
 </style>
